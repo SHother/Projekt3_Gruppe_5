2bfd9a7d62d936ac4a31dfcd59d4e3a0bfe0ddac
Soren Hother <sora0002@stud.kea.dk>
2026-04-28 15:23:44 +0200

    First commit

---
+++ src/main/java/org/example/projekt3_gruppe_5/Projekt3Gruppe5Application.java
@@ -0,0 +1,13 @@
+package org.example.projekt3_gruppe_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt3Gruppe5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt3Gruppe5Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projekt3_gruppe_5/Projekt3Gruppe5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projekt3_gruppe_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt3Gruppe5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
