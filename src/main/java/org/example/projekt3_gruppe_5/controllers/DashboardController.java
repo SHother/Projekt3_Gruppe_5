93a5e6f1ec06034759b11a241c884920642645ea
Søren <soren.hother@gmail.com>
2026-05-07 17:22:22 +0200

    Pipeline fra html til Sql med AllCar info

--- src/main/java/org/example/projekt3_gruppe_5/controllers/DashboardController.java
+++ src/main/java/org/example/projekt3_gruppe_5/controllers/DashboardController.java
@@ -1,6 +1,29 @@
 package org.example.projekt3_gruppe_5.controllers;
 
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import org.example.projekt3_gruppe_5.repositories.CarRepository;
+
+@Controller
 public class DashboardController {
+    private final CarRepository carRepository;
+
+    public DashboardController(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("cars", carRepository.allCars());
+        return "inventory";
+    }
+
 }
 
 //test
