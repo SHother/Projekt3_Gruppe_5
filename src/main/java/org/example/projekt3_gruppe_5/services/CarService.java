683aa36b320515fbf34430fd7f099c2422bcc0a2
Soren Hother <sora0002@stud.kea.dk>
2026-05-08 14:06:21 +0200

    tables of cars med mini filter

--- src/main/java/org/example/projekt3_gruppe_5/services/CarService.java
+++ src/main/java/org/example/projekt3_gruppe_5/services/CarService.java
@@ -1,4 +1,42 @@
 package org.example.projekt3_gruppe_5.services;
 
+import org.example.projekt3_gruppe_5.models.Car;
+import org.example.projekt3_gruppe_5.repositories.CarRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CarService {
+
+    private final CarRepository carRepository;
+
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public List<Car> filterCars(String brand, String status) {
+
+        List<Car> cars = getAllCars();
+
+        if (brand != null && !brand.isEmpty()) {
+            cars = cars.stream()
+                    .filter(c -> c.getBrand().toLowerCase()
+                            .contains(brand.toLowerCase()))
+                    .toList();
+        }
+
+        if (status != null && !status.isEmpty()) {
+            cars = cars.stream()
+                    .filter(c -> c.getStatus().equalsIgnoreCase(status))
+                    .toList();
+        }
+
+        return cars;
+    }
+
+    public List<Car> getAllCars() {
+        return carRepository.allCars();
+    }
+
 }
