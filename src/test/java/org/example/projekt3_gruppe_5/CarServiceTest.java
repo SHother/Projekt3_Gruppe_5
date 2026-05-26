708dbd8e1f5f218fb90913da384270382ab94804
MogensMickisen <72939792+LordGlarthir@users.noreply.github.com>
2026-05-26 20:59:52 +0200

    CarServiceTest Comments

--- src/test/java/org/example/projekt3_gruppe_5/CarServiceTest.java
+++ src/test/java/org/example/projekt3_gruppe_5/CarServiceTest.java
@@ -13,16 +13,21 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 class CarServiceTest {
-
+        //Ansvarlig: Micki
     private CarRepository carRepository;
     private CarService carService;
-
+        // Denne metode bliver kørt før hver test.
+        //Sætter et test miljø op ved at lave en "Mocker" gennem Mockito.
+        //Gør at testen ikke forbinder til vores aktuelle Azure Database.
     @BeforeEach
     void setUp() {
         carRepository = mock(CarRepository.class);
         carService = new CarService(carRepository);
     }
 
+        //Vores Happy flow, hvor vi kører en test der har opsatte parametre
+        //der er accepteret i vores system.
+        //i disse test scenarier er det kun bilens pris vi tester.
     @Test
     @DisplayName("TC2.1: Opret gyldig bil (Happy Flow)")
     void testSaveCarHappyFlow() {
@@ -35,6 +40,8 @@ class CarServiceTest {
         verify(carRepository, times(1)).insertCar(car);
     }
 
+        //Vores Exception Flow, hvor en test bliver sat op med
+        //parametre der er udenfor vores angivede prisramme i systemet
     @Test
     @DisplayName("TC2.3: Negativ pris (Exception Flow)")
     void testSaveCarNegativePrice() {
@@ -44,6 +51,7 @@ class CarServiceTest {
         car.setPrice(-500);
 
         BadRequestException exception = assertThrows(BadRequestException.class, () -> carService.saveCar(car));
+        //Herunder bliver der kontrolleret at der bliver kastet den korrekte fejlkode.
         assertEquals("Car price must be greater than 0", exception.getMessage());
         verify(carRepository, never()).insertCar(any());
     }
